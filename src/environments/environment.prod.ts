f7e923dbfdc3d7a3e23ef903787ee99c6d5c5674
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-08 16:25:08 +0300

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7054/'
+  apiUrl: 'https://localhost:7054/',
+  grafanaUrl: 'http://localhost:3000',
+  prometheusUrl: 'http://localhost:9090',
+  features: {
+    realTimeAnalytics: true,
+    aiIntegration: true,
+    eventSourcing: true
+  }
 };
