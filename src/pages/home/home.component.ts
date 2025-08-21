54aafe858e3a4c9bb4c165d32bc98753873f4415
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-21 16:32:11 +0300

    ok

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  
+export class HomeComponent implements OnInit {
+  categories: string[] = [];
+
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.firebaseService.getBookingTrips().subscribe(trips => {
+      const allCategories = trips.map(trip => trip.category).filter(Boolean);
+      this.categories = Array.from(new Set(allCategories));
+    });
+  }
 }
