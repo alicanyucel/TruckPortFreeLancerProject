801040658f32e5de33884510f334ba7ff512912b
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-22 12:14:58 +0300

    ...

--- src/components/live-map/live-map.component.ts
+++ src/components/live-map/live-map.component.ts
@@ -29,9 +29,12 @@ export class LiveMapComponent implements OnInit {
     { id: '001', vehicleType: '', date: '', loading: '', delivery: '', distance: '', owner: '' },
     { id: '002', vehicleType: '', date: '', loading: '', delivery: '', distance: '', owner: '' },
     { id: '003', vehicleType: '', date: '', loading: '', delivery: '', distance: '', owner: '' },
-    { id: '004', vehicleType: '', date: '', loading: '', delivery: '', distance: '', owner: '' },
-    { id: '005', vehicleType: '', date: '', loading: '', delivery: '', distance: '', owner: '' },
-    { id: '006', vehicleType: '', date: '', loading: '', delivery: '', distance: '', owner: '' }
+    { id: 'RES-001', vehicleType: 'Mercedes Actros', date: '2025-08-20 09:30', loading: 'İstanbul Avcılar', delivery: 'Ankara Etimesgut', distance: '450 km', owner: 'Ahmet Y.', image: 'https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg', ownerImage: 'https://randomuser.me/api/portraits/men/11.jpg' },
+    { id: 'RES-002', vehicleType: 'Volvo FH16', date: '2025-08-21 14:00', loading: 'İzmir Gaziemir', delivery: 'Bursa Nilüfer', distance: '170 km', owner: 'Mehmet K.', image: 'https://images.pexels.com/photos/248614/pexels-photo-248614.jpeg', ownerImage: 'https://randomuser.me/api/portraits/men/12.jpg' },
+    { id: 'RES-003', vehicleType: 'Scania R450', date: '2025-08-22 08:00', loading: 'İzmit', delivery: 'Gebze', distance: '45 km', owner: 'Ali V.', image: 'https://images.pexels.com/photos/163119/sport-car-vehicle-road-163119.jpeg', ownerImage: 'https://randomuser.me/api/portraits/men/13.jpg' },
+    { id: 'RES-004', vehicleType: 'MAN TGX', date: '2025-08-23 12:15', loading: 'Adana', delivery: 'Mersin', distance: '90 km', owner: 'Osman Ç.', image: 'https://images.pexels.com/photos/358070/pexels-photo-358070.jpeg', ownerImage: 'https://randomuser.me/api/portraits/men/14.jpg' },
+    { id: 'RES-005', vehicleType: 'DAF XF', date: '2025-08-24 07:45', loading: 'Antalya', delivery: 'Isparta', distance: '200 km', owner: 'Hasan B.', image: 'https://images.pexels.com/photos/1936675/pexels-photo-1936675.jpeg', ownerImage: 'https://randomuser.me/api/portraits/men/15.jpg' },
+    { id: 'RES-006', vehicleType: 'Iveco Stralis', date: '2025-08-25 16:30', loading: 'Samsun', delivery: 'Trabzon', distance: '450 km', owner: 'Kemal Ö.' , image: 'https://images.pexels.com/photos/1936676/pexels-photo-1936676.jpeg', ownerImage: 'https://randomuser.me/api/portraits/men/16.jpg' }
   ];
 
   pageSize = 3;
